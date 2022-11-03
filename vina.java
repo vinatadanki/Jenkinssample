b2564c1cc6e597fb09521e4719d2986173a99a0b
vinatadanki <95192885+vinatadanki@users.noreply.github.com>
2022-11-02 21:11:34 -0500

    Update vina.java

--- vina.java
+++ vina.java
@@ -1,5 +1,5 @@
 public class vina {
 public static void main(String[] args) {
-System.out.println("This is vina- triggered by webhook");
+System.out.println("This is vina- triggered by webhook again");
 }
 }
